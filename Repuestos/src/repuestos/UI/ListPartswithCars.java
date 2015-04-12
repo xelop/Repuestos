b336293b5ca07a395ad0f9fdd1b5896fdc40a56b
xelop <adrianlq8@hotmail.com>
2015-04-12 16:03:13 -0600

    Model con letras

--- Repuestos/src/repuestos/UI/ListPartswithCars.java
+++ Repuestos/src/repuestos/UI/ListPartswithCars.java
@@ -161,10 +161,6 @@ public class ListPartswithCars extends javax.swing.JFrame {
 
     private void _txt_ModelKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event__txt_ModelKeyReleased
         // TODO add your handling code here:
-        if(Character.isDigit(evt.getKeyChar())||evt.getExtendedKeyCode()==8) {
-        } else{
-            _txt_Model.setText(""+_txt_Model.getText().substring(0, _txt_Model.getText().length() - 1));
-        }
     }//GEN-LAST:event__txt_ModelKeyReleased
 
     private void _txt_YearKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event__txt_YearKeyReleased
